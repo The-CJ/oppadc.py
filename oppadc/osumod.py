12192c41392280122743713700373b09af3582b8
The_CJ <cj@tbits.net>
2019-10-23 14:04:20 +0200

    added general mods for std game

--- oppadc/osumod.py
+++ oppadc/osumod.py
@@ -5,4 +5,30 @@ class GeneralOsuMod(object):
 	"""
 	def __init__(self, value:int, name:str=None):
 		self.value:int = value
-		self.name = name or "[N/A]"
+		self.name:str = name or "[N/A]"
+
+	def __repr__(self) -> str:
+		return f"<{self.__class__.__name__} name='{self.name}' ({self.value})>"
+
+	def __str__(self) -> str:
+		return self.__repr__()
+
+index:dict = dict(
+	NM = GeneralOsuMod(0, "No Mod"),
+	NF = GeneralOsuMod(1, "No fail"),
+	EZ = GeneralOsuMod(1<<1, "Easy"),
+	TD = GeneralOsuMod(1<<2, "TouchDevice"),
+	HD = GeneralOsuMod(1<<3, "Hidden"),
+	HR = GeneralOsuMod(1<<4, "Hardrock"),
+	SD = GeneralOsuMod(1<<5, "Sudden death"),
+	DT = GeneralOsuMod(1<<6, "Double time"),
+	RX = GeneralOsuMod(1<<7, "Relax"),
+	HT = GeneralOsuMod(1<<8, "Half time"),
+	NC = GeneralOsuMod(1<<9, "Nightcore"),
+	FL = GeneralOsuMod(1<<10, "Flashlight"),
+	AP = GeneralOsuMod(1<<11, "Autoplay"),
+	SO = GeneralOsuMod(1<<12, "Spun out"),
+
+
+
+)
